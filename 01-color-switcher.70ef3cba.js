d28adfddf73434e96e762af4c7492f0195f45410
Serhii-Kysil <127446937+Serhii-Kysil@users.noreply.github.com>
2023-11-20 20:48:28 +0000

    Deploying to gh-pages from @ Serhii-Kysil/goit-js-hw-09@a30caf7220b75eac2d3344b69f4b521206a33374 🚀

--- 01-color-switcher.70ef3cba.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let d=null;t.addEventListener("click",(function(){this.disabled=!0,e.disabled=!1,d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(){this.disabled=!0,t.disabled=!1,clearInterval(d)}));
-//# sourceMappingURL=01-color-switcher.70ef3cba.js.map
