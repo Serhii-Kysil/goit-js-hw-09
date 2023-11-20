bff92f0a4c5b7934e15fcd9f5930a7d0f7c43274
Serhii-Kysil <serik.kisel000@gmail.com>
2023-11-20 19:42:03 +0200

    Task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const firstDelay = parseInt(form.elements['delay'].value);
+  const delayStep = parseInt(form.elements['step'].value);
+  const amount = parseInt(form.elements['amount'].value);
+
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, firstDelay + (i - 1) * delayStep)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
+});
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      const promiseResult = { position, delay };
+
+      if (shouldResolve) {
+        resolve(promiseResult);
+      } else {
+        reject(promiseResult);
+      }
+    }, delay);
+  });
 }
